ece4d6601cfa4005ffb57f5ec246b8775cc0ed4f
Jagshishsingh <43028921+Jagshishsingh@users.noreply.github.com>
2020-01-15 22:01:39 +0530

    google authentication bug cleared and firebase_ml_vision added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
-import 'package:flutter/material.dart';
-import 'package:firebase/firebase.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
@@ -21,13 +19,18 @@ class HomePage extends StatelessWidget {
   final GoogleSignIn googleSignIn = new GoogleSignIn();
 
   Future<FirebaseUser> _signIn() async{
-    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
     GoogleSignInAuthentication googleAuth  = await googleSignInAccount.authentication;
-    final AuthCredential credential = firebase_auth.GoogleAuthProvider.getCredential(
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
     idToken: googleAuth.idToken, accessToken: googleAuth.accessToken,
     );
-    final FirebaseUser  user = (await _auth.signInWithCredential(credential)).user;
+    final AuthResult authResult = await _auth.signInWithCredential(credential);
+    final FirebaseUser user = authResult.user;
     return user;
+  }
+  void _signOut(){
+    googleSignIn.signOut();
+    print("signed out");
   }
 
   @override
