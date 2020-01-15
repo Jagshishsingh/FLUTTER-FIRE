c99555cdc45d3dd6a1b488425e268c6a51f71be4
Jagshishsingh <43028921+Jagshishsingh@users.noreply.github.com>
2020-01-15 18:45:18 +0530

    google authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 import 'package:flutter/material.dart';
 import 'package:firebase/firebase.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 void main() => runApp(MyApp());
 
@@ -14,6 +16,20 @@ class MyApp extends StatelessWidget{
 }
 
 class HomePage extends StatelessWidget {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
+
+  Future<FirebaseUser> _signIn() async{
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth  = await googleSignInAccount.authentication;
+    final AuthCredential credential = firebase_auth.GoogleAuthProvider.getCredential(
+    idToken: googleAuth.idToken, accessToken: googleAuth.accessToken,
+    );
+    final FirebaseUser  user = (await _auth.signInWithCredential(credential)).user;
+    return user;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +43,7 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             new RaisedButton(
-              onPressed: null,
+              onPressed: ()=> _signIn().then((FirebaseUser user) => print(user)).catchError((e)=> print(e)),
               child: new Text("sign in"),
               color: Colors.green,
             ),
